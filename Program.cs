b13300d4b43c47c7f6ab6d0d512015799b7018f4
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-07-03 21:58:54 -0300

    feat: registering user

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using myfreelas.Data;
+using myfreelas.Dtos.User;
+using myfreelas.Mapper;
+using myfreelas.Models;
+using myfreelas.Repositories.User;
+using myfreelas.Services.User;
+using myfreelas.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IValidator<RequestRegisterUserJson>, RegisterUserValidator>();
+
+
 builder.Services.AddDbContext<Context>(
     options => options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
@@ -11,10 +24,18 @@ builder.Services.AddDbContext<Context>(
     )
 );
 
+builder.Services.AddScoped(provider => new AutoMapper.MapperConfiguration(cfg => {
+    cfg.AddProfile(new MappingProfile());
+}).CreateMapper());
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
