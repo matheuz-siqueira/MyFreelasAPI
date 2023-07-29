49763bdd361eb00ada5dc876694daca7588e2c01
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-07-29 17:54:21 -0300

    fix: removing installment repository

--- Dtos/Customer/ResponseCustomerJson.cs
+++ Dtos/Customer/ResponseCustomerJson.cs
@@ -6,7 +6,7 @@ namespace myfreelas.Dtos.Customer;
 public class ResponseCustomerJson
 {
     public string Id { get; set; }
-    public string Name { get; set; }    
+    public string Name { get; set; }
     public string Email { get; set; }
     public CustomerEnum Type { get; set; }
     public List<ResponseFreelaJson> Freelas { get; set; }
