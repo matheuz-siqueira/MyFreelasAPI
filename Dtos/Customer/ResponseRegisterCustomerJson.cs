8ba4f633df10c634c91f55738b5539e1623579ca
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-07-31 17:00:18 -0300

    feat: returning type customer

--- Dtos/Customer/ResponseRegisterCustomerJson.cs
+++ Dtos/Customer/ResponseRegisterCustomerJson.cs
@@ -5,8 +5,8 @@ namespace myfreelas.Dtos.Customer;
 public class ResponseRegisterCustomerJson
 {
     public string Id { get; set; }
-    public string Name { get; set; }    
-    public string Email { get; set; }  
-    public string PhoneNumber { get; set; } 
-    public CustomerEnum Type { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Type { get; set; }
 }
